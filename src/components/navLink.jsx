b05db7d59ef8a1e74d9ab7fec5a01f40650936c5
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-07-30 23:00:31 +0530

    Update navLink.jsx

--- src/components/navLink.jsx
+++ src/components/navLink.jsx
@@ -8,6 +8,7 @@ const NavLink = ({link}) => {
   const pathName=usePathname()
   // console.log(pathName);
   return (
+    //link to each nav items
     <Link className={`rounded p-1 ${pathName ===link.url&& "bg-black text-white"}`} href={link.url}>{link.title} </Link>
   )
 }
