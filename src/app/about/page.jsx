b5f9b9902ab4582829f3469259a69d717f44b748
Tharun Vikas SS <115332693+Tharunvikas55@users.noreply.github.com>
2024-08-14 22:41:21 +0530

    Update page.jsx

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -349,7 +349,7 @@ const AboutPage = () => {
           </div>
         </div>
         {/* SVG CONTAINER */}
-        <div className="hidden lg:block w-1/3 sticky top-0 z-30  ">
+        <div className="hidden lg:block w-1/3 sticky top-0 z-30  xl:w-1/2 ">
           <Brain scrollYProgress={scrollYProgress} />
         </div>
       </div>
